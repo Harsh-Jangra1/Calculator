b58aaa364ddfaeb188b881ab98f066ab6816c44a
Harsh-Jangra1 <harshjangra021@gmail.com>
2025-06-25 20:29:38 +0530

    add javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const input = document.getElementById('user-input');
+const btn = document.querySelectorAll('button');
+
+
+
+btn.forEach((btnKey) => {
+    btnKey.addEventListener('click', (e) => {
+        const value = e.target.textContent.trim();
+        if (value == 'Ac') {
+            input.value = '';
+        } else if (value == 'C') {
+            input.value = input.value.slice(0, -1);
+        } else if (value == '=') {
+            try {
+                input.value = eval(input.value);
+            } catch {
+                input.value = 'Error';
+            }
+        }
+    })
+})
