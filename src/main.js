8572df5c2b2957c0d695fb1d2e91878f790ebc55
Harsh-Jangra1 <harshjangra021@gmail.com>
2025-06-25 20:54:09 +0530

    add some css

--- src/main.js
+++ src/main.js
@@ -1,23 +1,69 @@
-'use strict';
+'use strict'
 
-const input = document.getElementById('user-input');
-const btn = document.querySelectorAll('button');
+let input = document.getElementById('user-input');
 
+document.querySelectorAll('.key-others').forEach((othersKey) => {
+    othersKey.addEventListener('click', (e) => {
+        const keyText = e.target.textContent.trim();
 
+        if (keyText === 'Ac') {
+            input.value = '0';
+        } else if (keyText === 'C') {
+            input.value = input.value.slice(0, - 1);
+        } else if (keyText === '%') {
 
-btn.forEach((btnKey) => {
-    btnKey.addEventListener('click', (e) => {
-        const value = e.target.textContent.trim();
-        if (value == 'Ac') {
-            input.value = '';
-        } else if (value == 'C') {
-            input.value = input.value.slice(0, -1);
-        } else if (value == '=') {
-            try {
-                input.value = eval(input.value);
-            } catch {
-                input.value = 'Error';
+            if (input.value !== '') {
+                try {
+                    input.value = eval(input.value) / 100;
+                } catch (error) {
+                    input.value = 'Error'
+                }
+            }
+        } else if (keyText == '=') {
+
+            if (input.value !== '') {
+                try {
+                    input.value = eval(input.value);
+                } catch (error) {
+                    input.value = 'Error';
+                }
             }
         }
     })
 })
+
+document.querySelectorAll('.number').forEach((numKey) => {
+    numKey.addEventListener('click', (evt) => {
+        const number = evt.target.textContent.trim();
+        if (input.value === '0') {
+            input.value = number;
+        } else {
+            input.value += number;
+        }
+    })
+})
+
+document.querySelectorAll('.key-operate').forEach((operatorKey) => {
+    operatorKey.addEventListener('click', (event) => {
+        const operator = event.target.textContent.trim();
+        const lastChar = input.value.slice(-1)
+        const validOperator = ['-', '+', '/', '*'];
+
+        if (input.value === '') {
+            if (operator === '-') {
+                input.value += operator;
+            }
+        } else if (!validOperator.includes(lastChar)) {
+            input.value += operator;
+        } else {
+            input.value = input.value.slice(0, -1) + operator;
+        }
+    })
+})
+
+document.querySelector('.decimal').addEventListener('click', () => {
+    const lastNum = input.value.split(/[\+\-\*\/]/).pop();
+    if (!lastNum.includes('.')) {
+        input.value += '.';
+    }
+});
